dc91a2af6656c5eee7fa964dddb51d2cdcf76e28
SardineMilk <152029833+SardineMilk@users.noreply.github.com>
2025-10-23 12:27:09 +0000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def on_button_pressed_a():
     global bird
-    bird += -1
+    if bird >= 0:
+        bird += -1
     music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
         music.PlaybackMode.IN_BACKGROUND)
 input.on_button_pressed(Button.A, on_button_pressed_a)
@@ -9,33 +10,31 @@ def startTheGame():
     global spawnPipe, bird, speed, pipes, pipePositionOdd
     spawnPipe = 1
     bird = 2
-    speed = 500
+    speed = 1000  
     pipes = [0]
     pipePositionOdd = 0
+    score = 0
     while True:
         basic.clear_screen()
-        bird += 1
+        if bird < 4:  
+            bird += 1
         led.plot_brightness(0, bird, 255)
-        speed += 0 - speed / 1000000000
-        if bird > 5:
-            bird = 2
+        speed -= 1
 
         if spawnPipe == 1:
             spawnPipe = 0
             pipes.append(randint(0, 3))
         else:
             spawnPipe = 1
-        
-        if (pipes.length > 3):
+        if len(pipes) > 3:
             pipes.remove_at(0)
-
         if pipePositionOdd == 1:
             pipePositionOdd = 0
         else:
             pipePositionOdd = 1
-
-        for i in range(pipes.length):
-            pipex = (i*2) + pipePositionOdd
+        i = 0
+        while i <= len(pipes) - 1:
+            pipex = i * 2 + pipePositionOdd
             pipey = pipes[i]
             led.plot(pipex, 0)
             led.plot(pipex, 1)
@@ -43,8 +42,14 @@ def startTheGame():
             led.plot(pipex, 3)
             led.plot(pipex, 4)
             led.unplot(pipex, pipey)
-            led.unplot(pipex, pipey+1)
+            led.unplot(pipex, pipey + 1)
+            i += 1
 
+        if (bird == pipes[0]) or (bird == pipes[0]+1):
+            score +=1
+        else:
+            score -= 1
+        basic.pause(speed)
 pipePositionOdd = 0
 pipes: List[number] = []
 speed = 0
--- main.ts
+++ main.ts
@@ -1,44 +1,38 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    bird += -1
+input.onButtonPressed(Button.A, function () {
+    if (bird >= 0) {
+        bird += -1
+    }
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
 })
-function startTheGame() {
-    let pipex: number;
-    let pipey: number;
-    
-    spawnPipe = 1
+function startTheGame () {
+    let i: number;
+let pipex: number;
+let pipey: number;
+spawnPipe = 1
     bird = 2
-    speed = 500
+    speed = 1000
     pipes = [0]
     pipePositionOdd = 0
-    while (true) {
+    running = 1
+    while (running == 1) {
         basic.clearScreen()
-        bird += 1
-        led.plotBrightness(0, bird, 255)
-        speed += 0 - speed / 1000000000
-        if (bird > 5) {
-            bird = 2
-        }
-        
+        speed += 0 - 1
         if (spawnPipe == 1) {
             spawnPipe = 0
             pipes.push(randint(0, 3))
         } else {
             spawnPipe = 1
         }
-        
         if (pipes.length > 3) {
             pipes.removeAt(0)
         }
-        
         if (pipePositionOdd == 1) {
             pipePositionOdd = 0
         } else {
             pipePositionOdd = 1
         }
-        
-        for (let i = 0; i < pipes.length; i++) {
+        i = 0
+        while (i <= pipes.length - 1) {
             pipex = i * 2 + pipePositionOdd
             pipey = pipes[i]
             led.plot(pipex, 0)
@@ -48,12 +42,31 @@ function startTheGame() {
             led.plot(pipex, 4)
             led.unplot(pipex, pipey)
             led.unplot(pipex, pipey + 1)
+            i += 1
         }
+        if (bird == pipes[0] || bird == pipes[0] + 1) {
+            score += 1
+        } else {
+            score += 0 - 1
+        }
+        led.plotBrightness(0, bird, 255)
+        basic.pause(speed)
     }
+    basic.showNumber(score)
 }
-
+input.onButtonPressed(Button.AB, function () {
+    running = 0
+})
+input.onButtonPressed(Button.B, function () {
+    if (bird < 4) {
+        bird += 1
+    }
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
+})
+let score = 0
+let running = 0
 let pipePositionOdd = 0
-let pipes : number[] = []
+let pipes: number[] = []
 let speed = 0
 let spawnPipe = 0
 let bird = 0
