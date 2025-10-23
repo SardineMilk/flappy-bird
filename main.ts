cebcc883753569061b20d30f5422319889a8a019
SardineMilk <152029833+SardineMilk@users.noreply.github.com>
2025-10-23 11:35:47 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-    bird += 1
+    bird += -1
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
 })
-input.onButtonPressed(Button.AB, function () {
-	
-})
-let bird = 2
-let speed = 500
-let pipes = [0]
-loops.everyInterval(speed, function () {
-    led.plotBrightness(0, bird, 255)
-    speed += 0 - speed / 1000
-})
+function startTheGame () {
+    bird = 2
+    speed = 500
+    pipes = [0]
+    while (true) {
+        basic.clearScreen()
+        bird += 1
+        led.plotBrightness(0, bird, 255)
+        speed += 0 - speed / 1000
+        if (bird > 5) {
+            bird = 2
+        }
+        basic.pause(speed)
+    }
+}
+let pipes: number[] = []
+let speed = 0
+let bird = 0
+startTheGame()
